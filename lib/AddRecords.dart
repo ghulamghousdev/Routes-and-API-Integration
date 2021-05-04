9e03350ce0b9534c4c74c693a8f65aaca15c8f4d
ghulamghousdev <itsghulamghous@gmail.com>
2021-05-04 09:25:49 +0500

    📦 New: minor changes

--- lib/AddRecords.dart
+++ lib/AddRecords.dart
@@ -18,6 +18,7 @@ Future createRecord(
   final res = await http.post(apiURL,
       headers: {
         'Content-Type': 'application/json; charset=UTF-8',
+        "Access-Control-Allow-Origin": "*",
       },
       body: jsonEncode(<String, String>{
         "firstName": first,
@@ -29,6 +30,7 @@ Future createRecord(
 
   if (res.statusCode == 201) {
     print('success');
+    return res;
   } else {
     print('fail');
     print(res);
@@ -45,163 +47,160 @@ class _AddRecordsState extends State<AddRecords> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(    
-      appBar: AppBar(
-        title: Text('Add Records'),
-      ),
-      drawer: Drawer(
-        child: ListView(
-          children: [
-            drawerpanel,
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Add Records'),
+        ),
+        drawer: Drawer(
+            child: ListView(children: [
+          drawerpanel,
           ListTile(
-          leading: Icon(Icons.person),
-          title: Text('HomePage',
-              style: TextStyle(
-                fontSize: 16,
-                color: Colors.grey[600],
-              )),
-              onTap: () {
-                Navigator.popUntil(context, ModalRoute.withName('/'));
-              },
-          ),
-      ListTile(
-          leading: Icon(Icons.person),
-          title: Text('Add Records',
-              style: TextStyle(
-                fontSize: 16,
-                color: Colors.grey[600],
-              )),
-          onTap: () {
-                Navigator.popAndPushNamed(context, '/addrecords');
-              },
-          ),
-      ListTile(
-        leading: Icon(Icons.article),
-        title: Text('View Records',
-            style: TextStyle(
-              fontSize: 16,
-              color: Colors.grey[600],
-            )),
+            leading: Icon(Icons.person),
+            title: Text('HomePage',
+                style: TextStyle(
+                  fontSize: 16,
+                  color: Colors.grey[600],
+                )),
             onTap: () {
-                Navigator.popAndPushNamed(context, '/viewrecords');
-              },
-      ),
-          ]
-          )
+              Navigator.popUntil(context, ModalRoute.withName('/'));
+            },
           ),
-     body: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          Column(children: <Widget>[
-            Padding(
-              padding: EdgeInsets.symmetric(horizontal: 26, vertical: 6),
-              child: new TextField(
-                decoration: new InputDecoration(
-                  border: OutlineInputBorder(),
-                  hintText: 'First Name',
-                ),
-                onChanged: (value) => {
-                  fName = value,
-                },
-              ),
-            )
-          ]),
-          Column(children: <Widget>[
-            Padding(
-              padding: EdgeInsets.symmetric(horizontal: 26, vertical: 6),
-              child: new TextField(
-                decoration: new InputDecoration(
-                  border: OutlineInputBorder(),
-                  hintText: 'Last Name',
-                ),
-                onChanged: (value) => {
-                  lName = value,
-                },
-              ),
-            )
-          ]),
-          Column(children: <Widget>[
-            Padding(
-              padding: EdgeInsets.symmetric(horizontal: 26, vertical: 6),
-              child: new TextField(
-                decoration: new InputDecoration(
-                  border: OutlineInputBorder(),
-                  hintText: 'Gender',
-                ),
-                onChanged: (value) => {
-                  sex = value,
-                },
-              ),
-            )
-          ]),
-          Column(children: <Widget>[
-            Padding(
-              padding: EdgeInsets.symmetric(horizontal: 26, vertical: 6),
-              child: new TextField(
-                decoration: new InputDecoration(
-                  border: OutlineInputBorder(),
-                  hintText: 'Email',
-                ),
-                onChanged: (value) => {
-                  emailAdress = value,
-                },
-              ),
-            )
-          ]),
-          Column(children: <Widget>[
-            Padding(
-              padding: EdgeInsets.symmetric(horizontal: 26, vertical: 6),
-              child: new TextField(
-                decoration: new InputDecoration(
-                  border: OutlineInputBorder(),
-                  hintText: 'Phone Number',
-                ),
-                onChanged: (value) => {
-                  phoneNum = value,
-                },
-              ),
-            )
-          ]),
-          Container(
-            margin: EdgeInsets.only(top: 10),
-            child: ElevatedButton(
-                child: Text('Add Records'),
-                style: ElevatedButton.styleFrom(
-                  onPrimary: Colors.white,
-                  elevation: 5,
-                ),
-                onPressed: () async {
-                  final String first = fName;
-                  final String last = lName;
-                  final String gndr = sex;
-                  final String eml = emailAdress;
-                  final String number = phoneNum;
+          ListTile(
+            leading: Icon(Icons.person),
+            title: Text('Add Records',
+                style: TextStyle(
+                  fontSize: 16,
+                  color: Colors.grey[600],
+                )),
+            onTap: () {
+              Navigator.popAndPushNamed(context, '/addrecords');
+            },
+          ),
+          ListTile(
+            leading: Icon(Icons.article),
+            title: Text('View Records',
+                style: TextStyle(
+                  fontSize: 16,
+                  color: Colors.grey[600],
+                )),
+            onTap: () {
+              Navigator.popAndPushNamed(context, '/viewrecords');
+            },
+          ),
+        ])),
+        body: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Column(children: <Widget>[
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 26, vertical: 6),
+                  child: new TextField(
+                    decoration: new InputDecoration(
+                      border: OutlineInputBorder(),
+                      hintText: 'First Name',
+                    ),
+                    onChanged: (value) => {
+                      fName = value,
+                    },
+                  ),
+                )
+              ]),
+              Column(children: <Widget>[
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 26, vertical: 6),
+                  child: new TextField(
+                    decoration: new InputDecoration(
+                      border: OutlineInputBorder(),
+                      hintText: 'Last Name',
+                    ),
+                    onChanged: (value) => {
+                      lName = value,
+                    },
+                  ),
+                )
+              ]),
+              Column(children: <Widget>[
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 26, vertical: 6),
+                  child: new TextField(
+                    decoration: new InputDecoration(
+                      border: OutlineInputBorder(),
+                      hintText: 'Gender',
+                    ),
+                    onChanged: (value) => {
+                      sex = value,
+                    },
+                  ),
+                )
+              ]),
+              Column(children: <Widget>[
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 26, vertical: 6),
+                  child: new TextField(
+                    decoration: new InputDecoration(
+                      border: OutlineInputBorder(),
+                      hintText: 'Email',
+                    ),
+                    onChanged: (value) => {
+                      emailAdress = value,
+                    },
+                  ),
+                )
+              ]),
+              Column(children: <Widget>[
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 26, vertical: 6),
+                  child: new TextField(
+                    decoration: new InputDecoration(
+                      border: OutlineInputBorder(),
+                      hintText: 'Phone Number',
+                    ),
+                    onChanged: (value) => {
+                      phoneNum = value,
+                    },
+                  ),
+                )
+              ]),
+              Container(
+                margin: EdgeInsets.only(top: 10),
+                child: ElevatedButton(
+                    child: Text('Add Records'),
+                    style: ElevatedButton.styleFrom(
+                      onPrimary: Colors.white,
+                      elevation: 5,
+                    ),
+                    onPressed: () async {
+                      final String first = fName;
+                      final String last = lName;
+                      final String gndr = sex;
+                      final String eml = emailAdress;
+                      final String number = phoneNum;
 
-                  final record =
-                      await createRecord(first, last, gndr, eml, number);
+                      final record =
+                          await createRecord(first, last, gndr, eml, number);
 
-
-                  return showDialog(
-                      context: context,
-                      builder: (BuildContext context) {
-                        return CupertinoAlertDialog(
-                          title: Text('Record Added',
-                              style: TextStyle(
-                                fontFamily: 'Raleway',
-                                fontSize: 24,
-                              )),
-                          actions: [
-                            TextButton(
-                                onPressed: () {
-                                  Navigator.pop(context);
-                                },
-                                child: Text('Ok'))
-                          ],
-                        );
-                      });
-                }),
-          )
-        ]));
+                      
+                      return showDialog(
+                          context: context,
+                          builder: (BuildContext context) {
+                            return CupertinoAlertDialog(
+                              title: Text('Record added Successfully',
+                                  style: TextStyle(
+                                    fontFamily: 'Raleway',
+                                    fontSize: 24,
+                                  )),
+                              actions: [
+                                TextButton(
+                                    onPressed: () {
+                                      Navigator.pop(context);
+                                    },
+                                    child: Text('Ok'))
+                              ],
+                            );
+                          });
+                    }),
+              )
+            ]));
   }
 }
--- lib/HomePage.dart
+++ lib/HomePage.dart
@@ -53,7 +53,8 @@ class HomePage extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            
+            //Image(image: NetworkImage("https://camo.githubusercontent.com/9b239b9541504aa3903d695155eb48db994437e8855c439c6f1f08b789e14d35/68747470733a2f2f6f63746f6465782e6769746875622e636f6d2f696d616765732f76696e796c746f6361742e706e67")),
+                 
             Text(
               'User Management Portal',
               style: TextStyle(fontSize: 20),
